e06a07f5bbc0fef88fab822092db5d7b7c0b703b
andrew_linux_laptop <andrewducdanguniv@gmail.com>
2024-02-12 14:08:24 -0800

    alice sends messages to bob with 6 bit gen

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from random import random
 from Crypto.Hash import SHA256
 from Crypto.Random import get_random_bytes
-from Crypto.Util import num
-from aes_cbc import cbc_encrypt, pkcs7, get_buf_pad
+from Crypto.Util import number
+from aes_cbc import cbc_encrypt, cbc_decrypt
 
+KEY_SIZE = 16
 
 def diffie(p, g):
     a_private = int(random()*p)
@@ -26,18 +27,17 @@ def diffie(p, g):
     return (a, b)
 
 def truncate(digest):
-    return bytearray(digest)[:16]
-
+    return bytearray(digest)[:KEY_SIZE]
 
 def exchange_6():
     p = 37
     g = 5
     (a, b) = diffie(p, g)
-    init_vector = get_random_bytes()
-    message = ""
-    for msg_block in get_buf_pad(message):
-        cbc_encrypt(msg_block, a, init_vector)
-        
+    init_vector = get_random_bytes(KEY_SIZE)
+    alice_bob_original = "Hello Bob! I am Alice. This message is long to test. I am writing now."
+    alice_bob_cipher = cbc_encrypt(alice_bob_original, a, init_vector)
+    alice_bob_decrypted = cbc_decrypt(alice_bob_cipher, b, init_vector)
+    print(alice_bob_decrypted)
 
 def exchange_ietf1024():
     pass
@@ -46,14 +46,11 @@ def rsa(m):
     #n modulo
     #d private
     e = 65537
-    p = num.getPrime(2048)
-    q = num.getPrime(2048)
+    p = number.getPrime(2048)
+    q = number.getPrime(2048)
     n = (p-1)* (q-1)
     crypt = pow(m,e,n)
     return crypt
     
-
-
-
 if __name__ == "__main__":
-    pass
+    exchange_6()
