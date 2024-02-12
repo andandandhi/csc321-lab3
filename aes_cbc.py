9907b9f1ca5e85491c4647f3b2cec9c7263aaea5
andrew_linux_laptop <andrewducdanguniv@gmail.com>
2024-02-12 13:45:38 -0800

    import and modify aes_cbc

--- aes_cbc.py
+++ aes_cbc.py
@@ -1,45 +1,7 @@
 from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-import sys
+from typing import Generator
 
 KEY_SIZE = 16
-#below is for task 2
-#aes_key = get_random_bytes(KEY_SIZE)
-#iv = get_random_bytes(KEY_SIZE)
-
-def file_reader() -> None:
-    aes_key = get_random_bytes(KEY_SIZE)
-    iv = get_random_bytes(KEY_SIZE)
-
-    n = len(sys.argv)
-    if(n < 1):
-        print("Missing file name")
-    
-    filename = sys.argv[1]
-    with open(filename, "rb") as in_file, \
-        open("ecb_ciphertext.bmp", "wb") as ecb_file:
-        
-        ecb_file.write(in_file.read(54)) #i dont know how big bmp header is; this is guess
-
-        buf = in_file.read(KEY_SIZE)
-        while len(buf) == KEY_SIZE:
-            ecb_file.write(ecb_encrypt(buf, aes_key))
-            buf = in_file.read(KEY_SIZE)
-        ecb_file.write(ecb_encrypt(pkcs7(buf), aes_key))
-
-#changes start here for CBC
-    with open(filename, "rb") as in_file, \
-        open("cbc_ciphertext.bmp", "wb") as cbc_file:
-        
-        cbc_file.write(in_file.read(54)) #i dont know how big bmp header is; this is guess
-
-        buf = in_file.read(KEY_SIZE)
-        while len(buf) == KEY_SIZE:
-            encrypted_buf = cbc_encrypt(buf, aes_key, iv)
-            cbc_file.write(encrypted_buf)
-            buf = in_file.read(KEY_SIZE)
-            iv = encrypted_buf
-        cbc_file.write(cbc_encrypt(pkcs7(buf), aes_key, iv))
 
 def pkcs7(buf: bytes) -> bytes:
     pad_len =  KEY_SIZE - len(buf)
@@ -47,19 +9,6 @@ def pkcs7(buf: bytes) -> bytes:
     for _ in range (pad_len):
         ba_buf.append(pad_len)
     return bytes(ba_buf)
-
-def ecb_encrypt(plaintextBlock: bytes, aes_key: bytes) -> bytes:
-    simpleCipher = AES.new(aes_key, AES.MODE_ECB)
-    encryptedBlock = simpleCipher.encrypt(plaintextBlock)
-    return(encryptedBlock)
-
-def conversion(word): #maybe change for just : and =
-    conversion = ""
-    for i in word:
-        con = ord(i)
-        conversion = conversion  + str(con)
-    conversion  = int(conversion)
-    return conversion
 
 def xor(words, iv):
     code = b""
@@ -70,34 +19,65 @@ def xor(words, iv):
         code = code + xor_val
     return code
 
-
-def cbc_encrypt(plaintextBlock: bytes, key: bytes, init_v: bytes) -> bytes:
-    
-    #plaintextBlock = get_random_bytes(16) #in bytes
-    #init_v = get_random_bytes(16) #in bytes
-    #key = get_random_bytes(KEY_SIZE) #generating new key as per diagram in bytes
-    xor_val = xor(plaintextBlock, init_v) #xoring plaintext and init vector
-    #print("xor val:") #checking xor, you can remove thes two lines
-    #print(xor_val)
-    
-    simpleCipher = AES.new(key, AES.MODE_ECB) #creating encrytpion alg
-   
-    
-    encryptedBlock = simpleCipher.encrypt(xor_val)#encrypting the xored value with ECB (so this is CBC)
-    #print("encrypted block:", encryptedBlock) #checking encryption
+def cbc_encrypt_block(plaintextBlock: bytes, key: bytes, prev_chain: bytes) -> bytes:
+    xor_val = xor(plaintextBlock, prev_chain)
+    simpleCipher = AES.new(key, AES.MODE_ECB)    
+    encryptedBlock = simpleCipher.encrypt(xor_val)
     return encryptedBlock
+
+def cbc_encrypt(plaintext: str, key: bytes, init_v: bytes):
+    byte_data = plaintext.encode('utf-8')
+    ciphertext = bytearray()
+    prev_cipherblock = init_v
+    for byte16 in get_buf_pad(byte_data):
+        cipherblock = cbc_encrypt_block(byte16, key, prev_cipherblock)
+        ciphertext.extend(cipherblock)
+        prev_cipherblock = cipherblock
+    return(bytes(ciphertext))
+
+def cbc_decrypt(ciphertext: bytes, key: bytes, init_v: bytes) -> str:
+    """Decrypts CBC ciphertext.
+    """
+    algorithm = AES.new(key, AES.MODE_ECB)    
+    decrypted_bytearray = bytearray()
+    prev_cipherblock = init_v
+    for cipherblock in get_buf(ciphertext):
+        aes_out = algorithm.decrypt(cipherblock)
+        plainblock = xor(aes_out, prev_cipherblock)
+        decrypted_bytearray.extend(plainblock)
+        prev_cipherblock = cipherblock
+    pad_remove = decrypted_bytearray[-1]
+    int(pad_remove)
+    decrypted_bytearray = decrypted_bytearray[:-pad_remove]
+    return decrypted_bytearray.decode('utf-8', errors='replace')
+
+
+def get_buf_pad(message: bytes) -> Generator[bytes, None, None]:
+    """Splits the message into 16 byte blocks, adding end padding.
+    """
+    buf = bytearray()
+    for b in message:
+        buf.append(b)
+        if len(buf) == 16:
+            encoding = bytes(buf)
+            yield encoding
+            buf = bytearray()
+    padding = pkcs7(bytes(buf))
+    yield padding
+
+def get_buf(message: bytes) -> Generator[bytes, None, None]:
+    """Splits the message into 16 byte blocks.
+    Len(input) must be multiple of 16
+    Does NOT remove padding. 
+    TODO: remove padding.
+    """
+    buf = bytearray()
+    for b in message:
+        buf.append(b)
+        if len(buf) == 16:
+            yield bytes(buf)
+            buf = bytearray()
+
     
-
-   #decryptedBlock = simpleCipher.decrypt(encryptedBlock) #checking decryption
-    #print(decryptedBlock)
-
-   
-
- 
-
-    
-
-if __name__ == '__main__':
-    file_reader()
     
 
