d4e61fae4541876818dd097193ebac4c24491121
Sharon Pagidipati <sp14517@gmail.com>
2024-02-12 20:08:45 -0800

    rsa changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Crypto.Hash import SHA256
 from Crypto.Random import get_random_bytes
 from Crypto.Util import number
 from aes_cbc import cbc_encrypt, cbc_decrypt
+from sympy import factor
 
 KEY_SIZE = 16
 
@@ -56,6 +57,20 @@ def rsa(m):
     print(crypt)
     return (crypt,n, phi)
 
+def hack_rsa():
+    #n modulo
+    #d private
+    e = 65537
+    #i = int(random() * 2048)
+    #j = int(random() * 2048)
+    p = 3
+    q = 7
+    n = p * q
+    num = factor(n)
+    print(num)
+   
+   
+
 def decrypt(crypt, n, phi):
     e = 65537
     d = pow(e, -1, phi) 
@@ -65,8 +80,9 @@ def decrypt(crypt, n, phi):
 
     
 if __name__ == "__main__":
-    crypt,n, phi = rsa(19)
-    decrypt(crypt, n, phi)
+    #crypt,n, phi = rsa(19)
+    #decrypt(crypt, n, phi)
+    hack_rsa()
 
 
     
