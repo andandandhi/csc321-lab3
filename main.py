ced433fa46a65c4ad8ec9c6f9cf354f844323a24
andandandhi <andrewducdanguniv@gmail.com>
2024-02-19 10:40:40 -0800

    HEX CONVERSION FINALLY WORKS

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def diffie_tamper_public(p, g):
 
 def diffie_tamper_g1(p, g):
     g = 1
-    return diffie(p, g)
+    (a, b) = diffie(p, g)
 
 def diffie_tamper_gp(p, g):
     g = p
@@ -75,21 +75,23 @@ def exchange_6():
     print(alice_bob_decrypted)
 
 def exchange_ietf1024():
-    p = bytes.fromhex(
-        'B10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6 \
+    p_hex = '0xB10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6 \
         9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C0 \
         13ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD70 \
         98488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0 \
         A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708 \
-        DF1FB2BC 2E4A4371')
+        DF1FB2BC 2E4A4371'.replace(" ", "")
+    p = int(p_hex, 0)
+    
 
-    g = bytes.fromhex(
-        'A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F \
+    g_hex ='0x\
+        A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F \
         D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213 \
         160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1 \
         909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A \
         D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24 \
-        855E6EEB 22B3B2E5')
+        855E6EEB 22B3B2E5'.replace(" ", "")
+    g = int(g_hex, 0)
 
     (a, b) = diffie(p, g)
     init_vector = get_random_bytes(KEY_SIZE)
@@ -156,6 +158,5 @@ if __name__ == "__main__":
     
     #alice, iv = hack_rsa(19)
     #mallory_decrypt(alice, iv)
-
     exchange_ietf1024()
     
