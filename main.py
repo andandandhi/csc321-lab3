d2d9d92eda516c84ef79e15ac9e20058901b463e
andandandhi <andrewducdanguniv@gmail.com>
2024-02-26 17:26:17 -0800

    begin g1 hack

--- main.py
+++ main.py
@@ -154,6 +154,36 @@ def exchange_ietf1024_hacked():
     mallory_stolen = cbc_decrypt(alice_bob_cipher, stolen_a, init_vector)
     print(mallory_stolen)
 
+def exchange_ietf1024_hacked_g1():
+    p_hex = '0xB10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6 \
+        9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C0 \
+        13ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD70 \
+        98488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0 \
+        A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708 \
+        DF1FB2BC 2E4A4371'.replace(" ", "")
+    p = int(p_hex, 0)
+    
+    g_hex ='0x\
+        A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F \
+        D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213 \
+        160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1 \
+        909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A \
+        D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24 \
+        855E6EEB 22B3B2E5'.replace(" ", "")
+    g = int(g_hex, 0)
+    (a, b) = diffie_tamper_g1(p, g)
+    init_vector = get_random_bytes(KEY_SIZE)
+    alice_bob_original = "Hello Bob! I am Alice. This message is long to test. I am writing now."
+    alice_bob_cipher = cbc_encrypt(alice_bob_original, a, init_vector)
+    #bytearray_print(a)
+    
+    s_mallory = 0
+    bytes_s_mallory = s_mallory.to_bytes(1, "big") 
+    mallory_SHA256 = SHA256.new()
+    mallory_SHA256.update(bytes_s_mallory)
+    stolen_a = truncate(mallory_SHA256.digest())
+    mallory_stolen = cbc_decrypt(alice_bob_cipher, stolen_a, init_vector)
+    print(mallory_stolen)
 
 
 def bytearray_print(x):
