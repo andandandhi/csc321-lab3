3b780a08f9c7baf363784ca3673a42a980a86157
andrewMINGW64 <andrewducdanguniv@gmail.com>
2024-03-01 12:05:10 -0800

    remove p - 1 bug

--- main.py
+++ main.py
@@ -20,16 +20,22 @@ def diffie(p, g):
     s_bob = pow(a_public, b_private, p)
 
     alice_SHA256 = SHA256.new()
-    s_alice_bytes_len = math.ceil(math.log(s_alice, 256))
+    if (s_alice == 0):
+        #edge-case
+        s_alice_bytes_len = 1
+    else:
+        s_alice_bytes_len = math.floor(math.log(s_alice, 256) + 1)
     bytes_s_alice = s_alice.to_bytes(s_alice_bytes_len, "big") 
     #bytearray_print(bytes_s_alice)
     alice_SHA256.update(bytes_s_alice)
 
     bob_SHA256 = SHA256.new()
-    s_bob_bytes_len = math.ceil(math.log(s_bob, 256))
+    if (s_bob == 0):
+        s_bob_bytes_len = 1
+    else:
+        s_bob_bytes_len = math.floor(math.log(s_bob, 256) + 1)
     bytes_s_bob = s_bob.to_bytes(s_bob_bytes_len, "big") 
     bob_SHA256.update(bytes_s_bob)
-
     a = truncate(alice_SHA256.digest())
     b = truncate(bob_SHA256.digest())
     return (a, b)
@@ -51,39 +57,25 @@ def diffie_tamper_public(p, g):
     s_bob = pow(a_public, b_private, p)
 
     alice_SHA256 = SHA256.new()
-    print(s_alice)
     if (s_alice == 0):
         #edge-case
         s_alice_bytes_len = 1
     else:
-        s_alice_bytes_len = math.ceil(math.log(s_alice, 256))
+        s_alice_bytes_len = math.floor(math.log(s_alice, 256) + 1)
     bytes_s_alice = s_alice.to_bytes(s_alice_bytes_len, "big") 
-    #bytearray_print(bytes_s_alice)
     alice_SHA256.update(bytes_s_alice)
 
     bob_SHA256 = SHA256.new()
     if (s_bob == 0):
         s_bob_bytes_len = 1
     else:
-        s_bob_bytes_len = math.ceil(math.log(s_bob, 256))
+        s_bob_bytes_len = math.floor(math.log(s_bob, 256) + 1)
     bytes_s_bob = s_bob.to_bytes(s_bob_bytes_len, "big") 
     bob_SHA256.update(bytes_s_bob)
 
     a = truncate(alice_SHA256.digest())
     b = truncate(bob_SHA256.digest())
     return (a, b)
-
-def diffie_tamper_g1(p, g):
-    g = 1
-    (a, b) = diffie(p, g)
-
-def diffie_tamper_gp(p, g):
-    g = p
-    return diffie(p, g)
-
-def diffie_tamper_g1(p, g):
-    g = p-1
-    return diffie(p, g)
 
 def truncate(digest):
     return bytearray(digest)[:KEY_SIZE]
@@ -171,11 +163,44 @@ def exchange_ietf1024_hacked_g1():
         D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24 \
         855E6EEB 22B3B2E5'.replace(" ", "")
     g = int(g_hex, 0)
-    (a, b) = diffie_tamper_g1(p, g)
+
+    g = 1 #TAMPER!!
+    (a, b) = diffie(p, g)
     init_vector = get_random_bytes(KEY_SIZE)
     alice_bob_original = "Hello Bob! I am Alice. This message is long to test. I am writing now."
     alice_bob_cipher = cbc_encrypt(alice_bob_original, a, init_vector)
-    #bytearray_print(a)
+    
+    s_mallory = 1
+    bytes_s_mallory = s_mallory.to_bytes(1, "big") 
+    mallory_SHA256 = SHA256.new()
+    mallory_SHA256.update(bytes_s_mallory)
+    stolen_a = truncate(mallory_SHA256.digest())
+    mallory_stolen = cbc_decrypt(alice_bob_cipher, stolen_a, init_vector)
+    print(mallory_stolen)
+
+def exchange_ietf1024_hacked_gp():
+    p_hex = '0xB10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6 \
+        9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C0 \
+        13ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD70 \
+        98488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0 \
+        A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708 \
+        DF1FB2BC 2E4A4371'.replace(" ", "")
+    p = int(p_hex, 0)
+    
+    g_hex ='0x\
+        A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F \
+        D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213 \
+        160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1 \
+        909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A \
+        D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24 \
+        855E6EEB 22B3B2E5'.replace(" ", "")
+    g = int(g_hex, 0)
+
+    g = p #TAMPER!!
+    (a, b) = diffie(p, g)
+    init_vector = get_random_bytes(KEY_SIZE)
+    alice_bob_original = "Hello Bob! I am Alice. This message is long to test. I am writing now."
+    alice_bob_cipher = cbc_encrypt(alice_bob_original, a, init_vector)
     
     s_mallory = 0
     bytes_s_mallory = s_mallory.to_bytes(1, "big") 
@@ -185,6 +210,37 @@ def exchange_ietf1024_hacked_g1():
     mallory_stolen = cbc_decrypt(alice_bob_cipher, stolen_a, init_vector)
     print(mallory_stolen)
 
+def exchange_ietf1024_hacked_gp1():
+    p_hex = '0xB10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6 \
+        9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C0 \
+        13ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD70 \
+        98488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0 \
+        A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708 \
+        DF1FB2BC 2E4A4371'.replace(" ", "")
+    p = int(p_hex, 0)
+    
+    g_hex ='0x\
+        A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F \
+        D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213 \
+        160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1 \
+        909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A \
+        D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24 \
+        855E6EEB 22B3B2E5'.replace(" ", "")
+    g = int(g_hex, 0)
+
+    g = p-1 #TAMPER!!
+    (a, b) = diffie(p, g)
+    init_vector = get_random_bytes(KEY_SIZE)
+    alice_bob_original = "Hello Bob! I am Alice. This message is long to test. I am writing now."
+    alice_bob_cipher = cbc_encrypt(alice_bob_original, a, init_vector)
+
+    s_mallory1 = 1
+    bytes_s_mallory1 = s_mallory1.to_bytes(1, "big") 
+    mallory_SHA256 = SHA256.new()
+    mallory_SHA256.update(bytes_s_mallory1)
+    stolen_a1 = truncate(mallory_SHA256.digest())
+    mallory_stolen1 = cbc_decrypt(alice_bob_cipher, stolen_a1, init_vector)
+    print(mallory_stolen1)
 
 def bytearray_print(x):
     print(''.join('\\{:02x}'.format(letter) for letter in x))
@@ -251,5 +307,5 @@ if __name__ == "__main__":
     
     #alice, iv = hack_rsa(19)
     #mallory_decrypt(alice, iv)
-    exchange_ietf1024_hacked()
+    exchange_ietf1024_hacked_gp1()
     
